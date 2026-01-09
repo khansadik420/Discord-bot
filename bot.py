c96791bb61b4b72d0247fd24187a33b8178d97a7
khansadik420 <82804910+khansadik420@users.noreply.github.com>
2026-01-09 18:38:38 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
+intents.message_content = True
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
@@ -14,3 +16,4 @@ async def ping(ctx):
     await ctx.send("Pong!")
 
 bot.run(os.getenv("TOKEN"))
+
